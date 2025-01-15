33f7a7eb2f00105e6c5314c599fa886441f0054b
MissShe <2224530706@qq.com>
2025-01-16 00:10:14 +0800

    Fix syntax errors

--- train_model.py
+++ train_model.py
@@ -47,7 +47,7 @@ def worker_init_fn(worker_id):
 def main_loop(batch_size=config.batch_size, model_type='', tensorboard=True):
     train_tf = transforms.Compose([RandomGenerator(output_size=[config.img_size, config.img_size])])
     val_tf = ValGenerator(output_size=[config.img_size, config.img_size])
-    if config.task_name == 'MosMedData-Adjust2':
+    if config.task_name == 'BUSI':
         train_text = read_text(config.train_dataset + 'Train_text.xlsx')
         val_text = read_text(config.val_dataset + 'Val_text.xlsx')
         train_dataset = ImageToImage2D(config.train_dataset, config.task_name, train_text, train_tf,
